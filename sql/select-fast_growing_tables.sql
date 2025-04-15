43b8a43db7578d0b13602e7d69e3a93b6131ae73
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-04-15 21:21:06 +0000

    #2 migrate files from data-sources 1195_monitor_db_size

---
+++ sql/select-fast_growing_tables.sql
@@ -0,0 +1,23 @@
+--noqa: disable=TMP
+WITH records AS (
+    SELECT
+        '`' || schema_name || '.' || relname || '`' AS obj_name,
+        dts,
+        table_sizes,
+        pg_size_pretty(table_sizes[1] - table_sizes[2]) AS size_change
+    FROM public.object_size_daily
+    WHERE
+        ABS(table_sizes[1] - table_sizes[2]) > pg_size_bytes('{{ var.json.db_monitoring.db_table_alert }}')  -->=10GB size increase in a day
+        AND dts[1] = CURRENT_DATE
+)
+
+SELECT
+    COUNT(*) < 1 AS _check,
+    'The following database object(s) changed in size by more than '
+    || '{{ var.json.db_monitoring.db_table_alert }} today:' AS summ,
+    array_agg(
+        obj_name || ' '
+        || CASE WHEN table_sizes[1] > table_sizes[2] THEN 'in' ELSE 'de' END || 'creased in size by `'
+        || size_change || '` between ' || dts[1] || ' and ' || dts[2] || '.'
+    ) AS gaps
+FROM records;
