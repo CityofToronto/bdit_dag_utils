62ecaab8a4e02a02d75dd43781a1ce9be1a968c0
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-04-17 14:58:01 +0000

    #2 add setup sql tables+functions

---
+++ sql/function-log_object_sizes_daily.sql
@@ -0,0 +1,52 @@
+-- FUNCTION: public.log_object_sizes_daily()
+
+-- DROP FUNCTION IF EXISTS public.log_object_sizes_daily();
+
+CREATE OR REPLACE FUNCTION public.log_object_sizes_daily(
+)
+RETURNS void
+LANGUAGE sql
+COST 100
+VOLATILE PARALLEL UNSAFE
+AS $BODY$
+
+INSERT INTO public.object_size_daily (schema_name, relname, table_sizes, dts)
+SELECT
+    pg_namespace.nspname AS schema_name,
+    pg_class.relname,
+    ARRAY[pg_total_relation_size(pg_class.oid)]::bigint[],
+    ARRAY[CURRENT_DATE]::date[]    
+FROM pg_catalog.pg_class
+JOIN pg_catalog.pg_namespace ON pg_class.relnamespace = pg_namespace.oid
+FULL JOIN public.object_size_daily AS existing
+    ON existing.schema_name = pg_namespace.nspname
+    AND existing.relname = pg_class.relname
+WHERE
+    pg_namespace.nspname NOT LIKE 'pg_%'
+    AND pg_total_relation_size(pg_class.oid) > 0
+ON CONFLICT ON CONSTRAINT object_size_daily_pkey
+--append to existing arrays
+DO UPDATE SET
+    table_sizes = EXCLUDED.table_sizes || object_size_daily.table_sizes,
+    dts = EXCLUDED.dts || object_size_daily.dts;
+
+--trim table_sizes ARRAYS
+UPDATE public.object_size_daily
+SET table_sizes = trim_array(table_sizes, array_length(table_sizes, 1)-10)
+WHERE array_length(table_sizes, 1) > 10;
+
+--trim dts ARRAYS
+UPDATE public.object_size_daily
+SET dts = trim_array(dts, array_length(dts, 1)-10)
+WHERE array_length(dts, 1) > 10;
+
+$BODY$;
+
+ALTER FUNCTION public.log_object_sizes_daily()
+OWNER TO dbadmin;
+
+GRANT EXECUTE ON FUNCTION public.log_object_sizes_daily() TO public;
+
+GRANT EXECUTE ON FUNCTION public.log_object_sizes_daily() TO dbadmin;
+
+GRANT EXECUTE ON FUNCTION public.log_object_sizes_daily() TO ref_bot;
---
+++ sql/function-log_schema_size_daily.sql
@@ -0,0 +1,40 @@
+-- FUNCTION: public.log_schema_size_daily()
+
+-- DROP FUNCTION IF EXISTS public.log_schema_size_daily();
+
+CREATE OR REPLACE FUNCTION public.log_schema_size_daily(
+)
+RETURNS void
+LANGUAGE sql
+COST 100
+VOLATILE PARALLEL UNSAFE
+AS $BODY$
+
+INSERT INTO public.schema_size_daily (
+    dt, schema_name, schema_size, schema_size_pretty
+)
+SELECT
+    CURRENT_DATE AS dt,
+    pg_namespace.nspname AS schema_name,
+    SUM(pg_total_relation_size(pg_class.oid)) AS schema_size,
+    pg_size_pretty(SUM(pg_total_relation_size(pg_class.oid))) AS schema_size_pretty
+FROM pg_catalog.pg_class
+JOIN pg_catalog.pg_namespace ON relnamespace = pg_namespace.oid
+GROUP BY
+    CURRENT_DATE,
+    pg_namespace.nspname
+ORDER BY schema_size DESC
+ON CONFLICT ON CONSTRAINT schema_size_daily_pkey
+DO UPDATE
+SET schema_size = EXCLUDED.schema_size,
+    schema_size_pretty = EXCLUDED.schema_size_pretty;
+$BODY$;
+
+ALTER FUNCTION public.log_schema_size_daily()
+OWNER TO dbadmin;
+
+GRANT EXECUTE ON FUNCTION public.log_schema_size_daily() TO public;
+
+GRANT EXECUTE ON FUNCTION public.log_schema_size_daily() TO dbadmin;
+
+GRANT EXECUTE ON FUNCTION public.log_schema_size_daily() TO ref_bot;
