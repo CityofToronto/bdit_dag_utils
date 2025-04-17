5f0a774f8e1b58f23ae99575eb28afc8dc26d90d
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-04-17 13:23:15 +0000

    #2 add coalesce to identify new tables

--- sql/select-fast_growing_tables.sql
+++ sql/select-fast_growing_tables.sql
@@ -4,10 +4,10 @@ WITH records AS (
         '`' || schema_name || '.' || relname || '`' AS obj_name,
         dts,
         table_sizes,
-        pg_size_pretty(table_sizes[1] - table_sizes[2]) AS size_change
+        pg_size_pretty(table_sizes[1] - coalesce(table_sizes[2],0)) AS size_change
     FROM public.object_size_daily
     WHERE
-        ABS(table_sizes[1] - table_sizes[2]) > pg_size_bytes('{{ var.json.db_monitoring.db_table_alert }}')  -->=10GB size increase in a day
+        ABS(table_sizes[1] - coalesce(table_sizes[2],0)) > pg_size_bytes('{{ var.json.db_monitoring.db_table_alert }}')  -->=10GB size increase in a day
         AND dts[1] = CURRENT_DATE
 )
 
@@ -17,7 +17,8 @@ SELECT
     || '{{ var.json.db_monitoring.db_table_alert }} today:' AS summ,
     array_agg(
         obj_name || ' '
-        || CASE WHEN table_sizes[1] > table_sizes[2] THEN 'in' ELSE 'de' END || 'creased in size by `'
-        || size_change || '` between ' || dts[1] || ' and ' || dts[2] || '.'
+        || CASE WHEN table_sizes[1] > coalesce(table_sizes[2],0) THEN 'in' ELSE 'de' END || 'creased in size by `'
+        || size_change || '` between ' || dts[1] || ' and ' || coalesce(dts[2], dts[1] - 1) || '.'
+        ORDER BY obj_name
     ) AS gaps
 FROM records;
