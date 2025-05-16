d7464f85cb6e20783f6a8d246a91b440a141e486
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-05-16 13:43:45 +0000

    update another get_current_context import #10

--- utils/common_tasks.py
+++ utils/common_tasks.py
@@ -61,7 +61,7 @@ def copy_table(conn_id:str, table:Tuple[str, ...], **context) -> None:
         )
     
     #name mapped task
-    from airflow.operators.python import get_current_context
+    from airflow.sdk import get_current_context
     context = get_current_context()
     context["dest_table_name"] = table[1]
     
