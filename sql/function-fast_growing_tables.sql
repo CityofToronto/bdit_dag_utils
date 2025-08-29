3e8bf1faebaca5f05d53b9a616643dac01eb1f9f
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2025-08-29 17:07:58 -0400

    #15 functionalize two select scripts

--- sql/function-fast_growing_tables.sql
+++ sql/function-fast_growing_tables.sql
@@ -1,24 +1,53 @@
---noqa: disable=TMP
-WITH records AS (
-    SELECT
-        '`' || schema_name || '.' || relname || '`' AS obj_name,
-        dts,
-        table_sizes,
-        pg_size_pretty(table_sizes[1] - coalesce(table_sizes[2],0)) AS size_change
-    FROM public.object_size_daily
-    WHERE
-        ABS(table_sizes[1] - coalesce(table_sizes[2],0)) > pg_size_bytes('{{ var.json.db_monitoring.db_table_alert }}')  -->=10GB size increase in a day
-        AND dts[1] = CURRENT_DATE
-)
+-- FUNCTION: public.fast_growing_tables(text)
 
-SELECT
-    COUNT(*) < 1 AS _check,
-    'The following database object(s) changed in size by more than '
-    || '{{ var.json.db_monitoring.db_table_alert }} today:' AS summ,
-    array_agg(
-        obj_name || ' '
-        || CASE WHEN table_sizes[1] > coalesce(table_sizes[2],0) THEN 'in' ELSE 'de' END || 'creased in size by `'
-        || size_change || '` between ' || dts[1] || ' and ' || coalesce(dts[2], dts[1] - 1) || '.'
-        ORDER BY obj_name
-    ) AS gaps
-FROM records;
+-- DROP FUNCTION IF EXISTS public.fast_growing_tables(text);
+
+CREATE OR REPLACE FUNCTION public.fast_growing_tables(
+    db_table_alert text
+)
+RETURNS TABLE(_check boolean, summ text, gaps text[]) 
+LANGUAGE SQL
+COST 100
+VOLATILE PARALLEL UNSAFE
+ROWS 1
+
+AS $BODY$
+    --noqa: disable=TMP
+    WITH records AS (
+        SELECT
+            '`' || schema_name || '.' || relname || '`' AS obj_name,
+            dts,
+            table_sizes,
+            pg_size_pretty(table_sizes[1] - coalesce(table_sizes[2],0)) AS size_change
+        FROM public.object_size_daily
+        WHERE
+            ABS(table_sizes[1] - coalesce(table_sizes[2],0)) > pg_size_bytes(fast_growing_tables.db_table_alert)  -->=10GB size increase in a day
+            AND dts[1] = CURRENT_DATE
+    )
+
+    SELECT
+        COUNT(*) < 1 AS _check,
+        'The following database object(s) changed in size by more than '
+        || fast_growing_tables.db_table_alert || ' today:' AS summ,
+        array_agg(
+            obj_name || ' '
+            || CASE WHEN table_sizes[1] > coalesce(table_sizes[2],0) THEN 'in' ELSE 'de' END || 'creased in size by `'
+            || size_change || '` between ' || dts[1] || ' and ' || coalesce(dts[2], dts[1] - 1) || '.'
+            ORDER BY obj_name
+        ) AS gaps
+    FROM records;
+
+$BODY$;
+
+--ptc
+ALTER FUNCTION public.fast_growing_tables(text) OWNER TO postgres;
+GRANT EXECUTE ON FUNCTION public.fast_growing_tables(text) TO postgres;
+GRANT EXECUTE ON FUNCTION public.fast_growing_tables(text) TO ptc_humans;
+
+--bigdata
+ALTER FUNCTION public.fast_growing_tables(text) OWNER TO dbadmin;
+GRANT EXECUTE ON FUNCTION public.fast_growing_tables(text) TO dbadmin;
+GRANT EXECUTE ON FUNCTION public.fast_growing_tables(text) TO bdit_humans;
+
+--both
+GRANT EXECUTE ON FUNCTION public.fast_growing_tables(text) TO ref_bot;
