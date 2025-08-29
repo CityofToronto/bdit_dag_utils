ac57c372ae233a95a919a84142cf2f7e56194313
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2025-08-29 17:08:16 -0400

    enable multi server monitoring #15

--- dags/monitor_db_size.py
+++ dags/monitor_db_size.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import socket
 import logging
 import pendulum
 from datetime import timedelta
@@ -23,6 +24,33 @@ doc_md = ""
 DAG_NAME = 'monitor_db_size'
 DAG_OWNERS = Variable.get('dag_owners', deserialize_json=True).get(DAG_NAME, ["Unknown"]) 
 
+HOST = socket.gethostname()
+if HOST == 'bancroft':
+    deployments = {
+        'bancroft': {
+            "conn_id": "ref_bot",
+            "var": "db_monitoring"
+        }
+    }
+elif HOST == 'morbius':
+    deployments = {
+        'morbius': {
+            "conn_id": "ref_bot_morbius",
+            "var": "db_monitoring_morbius"
+        },
+        'sirius': {
+            "conn_id": "ref_bot_sirius",
+            "var": "db_monitoring_sirius"
+        }
+    }
+else: #EC2
+    deployments = {
+        'ec2': {
+            "conn_id": "ref_bot",
+            "var": "db_monitoring"
+        }
+    }
+
 default_args = {
     'owner': ','.join(DAG_OWNERS),
     'depends_on_past':False,
@@ -34,38 +62,47 @@ default_args = {
     'on_failure_callback': task_fail_slack_alert
 }
 
-@dag(
-    DAG_NAME, 
-    default_args=default_args, 
-    schedule='0 8 * * *',
-    doc_md = doc_md,
-    tags=["bdit_dag_utils", "monitoring"],
-    template_searchpath=os.path.join(repo_path, 'sql'),
-    catchup=False
-)
-
-def monitor_db_size():
-    log_size_daily = SQLExecuteQueryOperator(
-        sql=["SELECT public.log_schema_size_daily();",
-             "SELECT public.log_object_sizes_daily();"],
-        task_id='log_size_daily',
-        conn_id='ref_bot',
-        autocommit=True,
-        retries = 0
+def create_monitoring_dag(dag_id, dag_conn_id, dag_var_id):
+    @dag(
+        dag_id,
+        default_args=default_args,
+        schedule='0 8 * * *',
+        doc_md = doc_md,
+        tags=["bdit_dag_utils", "monitoring"],
+        template_searchpath=os.path.join(repo_path, 'sql'),
+        catchup=False
     )
+
+    def monitor_db_size():
+        
+        log_size_daily = SQLExecuteQueryOperator(
+            sql=["SELECT public.log_schema_size_daily();",
+                "SELECT public.log_object_sizes_daily();"],
+            task_id='log_size_daily',
+            conn_id=dag_conn_id,
+            autocommit=True,
+            retries = 0
+        )
+        
+        check_fast_growing_tables = SQLCheckOperatorWithReturnValue(
+            task_id="check_fast_growing_tables",
+            sql=f"SELECT public.fast_growing_tables('{{{{ var.json.{dag_var_id}.db_table_alert }}}}'::text);",
+            conn_id=dag_conn_id
+        )
+
+        check_fast_growing_db = SQLCheckOperatorWithReturnValue(
+            task_id="check_fast_growing_db",
+            sql=f"SELECT public.check_db_growth('{{{{ var.json.{dag_var_id}.db_max_size }}}}'::text);",
+            conn_id=dag_conn_id
+        )
+
+        log_size_daily >> [check_fast_growing_tables, check_fast_growing_db]
+
+    generated_dag = monitor_db_size()
+    return generated_dag
     
-    check_fast_growing_tables = SQLCheckOperatorWithReturnValue(
-        task_id="check_fast_growing_tables",
-        sql="select-fast_growing_tables.sql",
-        conn_id="ref_bot"
+for server in deployments:
+    dag_id = f"{DAG_NAME}_{server}"
+    globals()[dag_id] = create_monitoring_dag(
+        dag_id, deployments[server]["conn_id"], deployments[server]["var"]
     )
-
-    check_fast_growing_db = SQLCheckOperatorWithReturnValue(
-        task_id="check_fast_growing_db",
-        sql="select-check_db_growth.sql",
-        conn_id="ref_bot"
-    )
-
-    log_size_daily >> [check_fast_growing_tables, check_fast_growing_db]
-
-monitor_db_size()
